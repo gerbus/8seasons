02ebcf846b4a5acd1066ee8c3731dd50487473ca
gerbus <gerbus3@gmail.com>
2018-03-12 00:33:34 +0000

    Cleaned house. Improved 8seasons library.

--- src/8seasons.js
+++ src/8seasons.js
@@ -3,61 +3,20 @@
 const Solstice = require('../node_modules/astronomia/lib/solstice.js');
 const Julian = require('../node_modules/julian/index.js');
 
+const name = ["Winter 1","Winter 2","Spring 1","Spring 2","Summer 1","Summer 2","Autumn 1","Autumn 2"];
+const samiName = ["Winter","Late Winter","Spring","Early Summer","Summer","Late Summer","Autumn","Early Winter"];
+
 export function get8Season(date) {
-  switch (get8SeasonIndex(date)) {
-    case 0:
-      return "Winter 1";
-      break;
-    case 1:
-      return "Winter 2";
-      break;
-    case 2:
-      return "Spring 1";
-      break;
-    case 3:
-      return "Spring 2";
-      break;
-    case 4:
-      return "Summer 1";
-      break;
-    case 5:
-      return "Summer 2";
-      break;
-    case 6:
-      return "Autumn 1";
-      break;
-    case 7:
-      return "Autumn 2";
-      break;
-  }
+  let data = get8SeasonInfo(date);
+  return data.name;
+}
+export function getNext8Season(date) {
+  let data = get8SeasonInfo(date);
+  return name[data.index + 1];
 }
 export function getSamiSeason(date) {
-  switch (get8SeasonIndex(date)) {
-    case 0:
-      return "Winter";
-      break;
-    case 1:
-      return "Late Winter";
-      break;
-    case 2:
-      return "Spring";
-      break;
-    case 3:
-      return "Early Summer";
-      break;
-    case 4:
-      return "Summer";
-      break;
-    case 5:
-      return "Late Summer";
-      break;
-    case 6:
-      return "Autumn";
-      break;
-    case 7:
-      return "Early Winter";
-      break;
-  }
+  let data = get8SeasonInfo(date);
+  return data.samiName;
 }
 
 export function getWinter1(year) {
@@ -94,27 +53,61 @@ export function getAutumn2(year) {
   return new Date(autumn1.getTime() + (winter1.getTime() - autumn1.getTime())/2);
 }
 
-function get8SeasonIndex(date) {
+export function get8SeasonInfo(date) {
   let year = date.getFullYear();
+  let winter2Start = getWinter2(year);
+  let spring1Start = getSpring1(year);
+  let spring2Start = getSpring2(year);
+  let summer1Start = getSummer1(year);
+  let summer2Start = getSummer2(year);
+  let autumn1Start = getAutumn1(year);
+  let autumn2Start = getAutumn2(year);
+  let winter1Start = getWinter1(year);
   
-  // Run through dates and return correct index
-  if (date >= getWinter1(year)) {
-    return 0;
-  } else if (date >= getAutumn2(year)) {
-    return 7;
-  } else if (date >= getAutumn1(year)) {
-    return 6;
-  } else if (date >= getSummer2(year)) {
-    return 5;
-  } else if (date >= getSummer1(year)) {
-    return 4;
-  } else if (date >= getSpring2(year)) {
-    return 3;
-  } else if (date >= getSpring1(year)) {
-    return 2;
-  } else if (date >= getWinter2(year)) {
-    return 1;
-  } else {
-    return 0;
+  let data = {};
+  // Run (backwards) through dates and return correct info
+  if (date >= winter1Start) { // Today is in winter 1, near the end of the year
+    data.index = 0;
+    data.seasonStart = winter1Start;
+    data.seasonEnd = getWinter2(year + 1);  // the end of winter 1 will be next year
+  } else if (date >= autumn2Start) {
+    data.index = 7;
+    data.seasonStart = autumn2Start;
+    data.seasonEnd = winter1Start;
+  } else if (date >= autumn1Start) {
+    data.index = 6;
+    data.seasonStart = autumn1Start;
+    data.seasonEnd = autumn2Start;
+  } else if (date >= summer2Start) {
+    data.index = 5;
+    data.seasonStart = summer2Start;
+    data.seasonEnd = autumn1Start;
+  } else if (date >= summer1Start) {
+    data.index = 4;
+    data.seasonStart = summer1Start;
+    data.seasonEnd = summer1Start;
+  } else if (date >= spring2Start) {
+    data.index = 3;
+    data.seasonStart = spring2Start;
+    data.seasonEnd = summer1Start;
+  } else if (date >= spring1Start) {
+    data.index = 2;
+    data.seasonStart = spring1Start;
+    data.seasonEnd = spring2Start;
+  } else if (date >= winter2Start) {
+    data.index = 1;
+    data.seasonStart = winter2Start;
+    data.seasonEnd = spring1Start;
+  } else {  // Today is in winter 1, near the beginning of the year
+    data.index = 0;
+    data.seasonStart = getWinter1(year - 1);  // the start of winter 1 was last year
+    data.seasonEnd = winter2Start;
   }
+  
+  data.seasonName = name[data.index];
+  data.nextSeasonName = name[(data.index + 1) % 8]
+  data.seasonNameSami = samiName[data.index];
+  data.nextSeasonNameSami = samiName[(data.index + 1) % 8]
+  
+  return data;
 }
--- src/App.js
+++ src/App.js
@@ -1,161 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Chart from 'chart.js';
+import moment from 'moment';
 import SunCalc from 'suncalc';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Meeus from '../node_modules/astronomia/lib/base.js';
 import * as EightSeasons from './8seasons.js';
-
-const pallette = {
-  winter1: "#8888ff",
-  winter2: "#8888cc",
-  spring1: "#88ff88",
-  spring2: "#ccff88",
-  summer1: "#ffffcc",
-  summer2: "#ffff88",
-  autumn1: "#ff4400",
-  autumn2: "#ff8800"
-};
 
 class App extends Component {
   constructor(props) {
     super(props);
-    Chart.defaults.global.elements.point.radius = 0;
+    this.state = {
+      currentDate: '',
+      current8season: '',
+      current8seasonStart: '',
+      current8seasonEnd: '',
+      currentDaylightHours: '',
+      currentSunrise: '',
+      currentSunset: '',
+      next8season: ''
+    };
+    this.handleTime = this.handleTime.bind(this);
+  }
+  componentWillMount() {
+    let today = new Date();
+    let eightSeasonData = EightSeasons.get8SeasonInfo(today);
+    let sunData =  SunCalc.getTimes(today,49.2827,-123.1207);
+    let daylightMilliseconds = sunData.sunset - sunData.sunrise;
+    
+    this.setState({
+      currentTime: moment(today).format("YYYY-MM-DD HH:mm:ss"),
+      current8season: eightSeasonData.seasonName,
+      current8seasonStart: moment(eightSeasonData.seasonStart).format("YYYY-MM-DD HH:mm"),
+      current8seasonEnd: moment(eightSeasonData.seasonEnd).format("YYYY-MM-DD HH:mm"),
+      next8season: eightSeasonData.nextSeasonName,
+      currentDaylightHours: moment.duration(daylightMilliseconds).hours() + " hours " + moment.duration(daylightMilliseconds).minutes() + " minutes",
+      currentSunrise: moment(sunData.sunrise).format("HH:mm"),
+      currentSunset: moment(sunData.sunset).format("HH:mm")
+    })
+    
+    setInterval(this.handleTime, 1000);
+  }
+  handleTime() {
+    let today = new Date();
+    this.setState({currentTime: moment(today).format("YYYY-MM-DD HH:mm:ss")});
   }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">The 8 Season Year</h1>
-        </header>
-        <div className="container">
-          <EightSeasonYear />
-        </div>
+    return(
+      <div>
+        <ul>
+          <li>{this.state.currentTime}</li>
+          <li>{this.state.current8season}</li>
+          <li>{this.state.current8seasonStart} to {this.state.current8seasonEnd}</li>
+          <li>{this.state.next8season}</li>
+          <li>{this.state.currentSunrise}</li>
+          <li>{this.state.currentSunset}</li>
+          <li>{this.state.currentDaylightHours}</li>
+        </ul>
       </div>
     );
   }
 }
 
-class EightSeasonYear extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      days: 365*2,
-      data: {}
-    };
-  }
-  
-  getDaylightHoursData() {
-    var start_date = new Date();
-    start_date.setDate(start_date.getDate() - Math.floor(this.state.days/2));
-    
-    var data = {
-      labels: [],
-      datasets: []
-    };
-    
-    // Initialize 8Season
-    let lastInterval = "";
-    let activeIntervalIndex = -1; 
-    
-    // Count number of 8seasons in timeframe (create a dataset for each)
-    //var n8Seasons = this.get8SeasonCount(start_date);
-   
-    // Iterate through all data points in range
-    for (let i = 0; i < this.state.days; i++) {
-      let date = new Date(start_date);
-      date.setDate(date.getDate() + i);
-      
-      // Set active 8season
-      let active8Season = EightSeasons.get8Season(date);
-      let activeInterval = date.getFullYear() + " - " + active8Season;
-      if (lastInterval != activeInterval) {
-        // Push new 8Season
-        activeIntervalIndex++;
-        
-        data.datasets.push(new Object());
-        data.datasets[activeIntervalIndex].label = activeInterval;
-        data.datasets[activeIntervalIndex].backgroundColor = pallette[active8Season.replace(" ","").toLowerCase()];
-        data.datasets[activeIntervalIndex].pointBackgroundColor = []; 
-        data.datasets[activeIntervalIndex].pointRadius = [];
-        data.datasets[activeIntervalIndex].data = [];
-
-        // Back fill previous days not in this season
-        for (let prevDay = 0; prevDay < i; prevDay++) {
-          data.datasets[activeIntervalIndex].data.push(null); 
-          data.datasets[activeIntervalIndex].pointBackgroundColor.push("#000000");
-          data.datasets[activeIntervalIndex].pointRadius.push(0);
-        }
-        
-        lastInterval = activeInterval;
-      }
-      
-      // Push sunlight data for today
-      data.labels.push(date);
-      let sunData =  SunCalc.getTimes(date,49.2827,-123.1207);
-      let daylightHours = (sunData.sunset - sunData.sunrise) / (1000 * 3600);
-      data.datasets[activeIntervalIndex].data.push(daylightHours);
-      
-      // Today?
-      if (i === Math.floor(this.state.days/2)) {  
-        // Style Today differently
-        data.datasets[activeIntervalIndex].pointBackgroundColor.push("#ff0000");
-        data.datasets[activeIntervalIndex].pointRadius.push(8);
-      } else {
-        data.datasets[activeIntervalIndex].pointBackgroundColor.push("#000000");
-        data.datasets[activeIntervalIndex].pointRadius.push(0);
-      }
-      
-    }
-    console.log(data);
-    return data;
-  }
-  get8SeasonCount(start_date) {
-    // How many intervals?
-    let lastInterval = "";
-    let intervalCount = 0;
-    
-    for (let i = 0; i < this.state.days; i++) {
-      // Set date
-      let date = new Date(start_date);
-      date.setDate(date.getDate() + i);
-
-      // Get year, season
-      let interval = date.getFullYear() + EightSeasons.get8Season(date);
-
-      // Incremet counter if new season
-      if (lastInterval != interval) {
-        intervalCount++;
-        //console.log(interval);
-        lastInterval = interval;
-      }
-    }
-    return intervalCount;
-  }
-  componentWillMount() {
-    this.setState({data: this.getDaylightHoursData()});
-  }
-  render() {
-    console.log(EightSeasons.getSamiSeason(new Date()));
-    return(
-      <canvas ref="myChart" id="myChart" width="400" height="200"></canvas>
-    );
-  }
-  componentDidMount() {
-    var myChart = new Chart(this.refs["myChart"], {
-      type: 'line',
-      data: this.state.data,
-      options: {
-        scales: {
-            yAxes: [{}]
-        }
-      }
-    });
-  }
-  componentDidUpdate() {
-    //myChart.datasets[0].points[this.today_index].fillColor = "#ff0000"; 
-  }
-}
 
 export default App;
