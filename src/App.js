41f68c04cb00ccecaec2fb05a8d8da5c54713e3b
gerbus <christopher.x.gerber@gmail.com>
2021-09-14 21:03:04 -0700

    Rotating earth background

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import React, { Component } from 'react';
-//import moment from 'moment';
-//import SunCalc from 'suncalc';
-//import eightSeason from './8seasons.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
+import rotatingEarth from './images/earth_night_rotate_1080.h264_10fps_.30_speed_cropped_before_minterpolate_smoothed_2_cropped.mp4'
+import rotatingEarthFrame1 from './images/frame_1.jpg'
 import ProximateSeasons from './components/ProximateSeasons';
 
-/** The main App */
 class App extends Component {
-  /*constructor(props) {
-    super(props);
-    this.state = {
-      current8season: null,
-      currentDaylightHours: null,
-      currentSunrise: null,
-      currentSunset: null
-    };
-  }*/
-  /*componentWillMount() {
-    let today = new Date();
-    let sunData =  SunCalc.getTimes(today,49.2827,-123.1207);
-    let daylightMilliseconds = sunData.sunset - sunData.sunrise;
-    
-    this.setState({
-      current8season: new eightSeason(today),
-      currentDaylightHours: moment.duration(daylightMilliseconds).hours() + " hours " + moment.duration(daylightMilliseconds).minutes() + " minutes",
-      currentSunrise: moment(sunData.sunrise).format("HH:mm"),
-      currentSunset: moment(sunData.sunset).format("HH:mm")
-    })
-  }*/
   render() {
     return(
-      <div className="container">
-        <div className="mt-3">
-          <h1>The Eight Season Year</h1>
-          <p>There's more to the changing of seasons than you think.</p>
+      <div classname="main">
+        <div className="background">
+          <video
+            muted
+            autoPlay
+            playsinline
+            loop
+            poster={rotatingEarthFrame1}
+            id="schedule-shifts"
+            preload
+            >
+            <source src={rotatingEarth} type="video/mp4" />
+            {/* <source src={scheduleShiftsWebm} type="video/webm" />
+            <source src={scheduleShiftsOgg} type="video/ogg" /> */}
+            Video unavailable
+          </video>
         </div>
-        <div className="seasons">
-          <ProximateSeasons />
+        <div className="container">
+          <div className="mt-3">
+            <h1>The Eight Season Year</h1>
+            <p>There's more to the changing of seasons than you think.</p>
+          </div>
+          <div className="seasons">
+            <ProximateSeasons />
+          </div>
         </div>
       </div>
     );
